8f1a437079624ad8527ed445d7ca6f3a915c71a8
ihsyamdev <ihsotas.yamasaki.dev@gmail.com>
2024-11-18 04:07:16 +0900

    ロゴ画像を差し替え

--- resources/js/components/Logo.tsx
+++ resources/js/components/Logo.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import logo from '../../images/logo.png'
+import logo from '../../images/logo_horizontal.png'
 import { BrowserRouter as Router, Link } from 'react-router-dom'
 
 export const Logo: React.FC = () => {
   return (
     <Router>
       <Link to='/'>
-        <img className='w-16 h-16' src={ logo } alt='logo' />
+        <img className='h-24' src={ logo } alt='logo' />
       </Link>
     </Router>
   )
