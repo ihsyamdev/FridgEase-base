15234bca8ee312f0540b1963d83d9c26f4553d5b
ihsyamdev <ihsotas.yamasaki.dev@gmail.com>
2024-11-18 16:11:27 +0900

    環境整理

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+    './resources/**/*.jsx',
+    './resources/**/*.ts',
+    './resources/**/*.tsx',
+  ],
   theme: {
     extend: {},
   },
