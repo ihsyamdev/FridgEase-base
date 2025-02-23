923ad2f2c96010cc7ffb1e8390ba190db9f738d8
ihsyamdev <ihsotas.yamasaki.dev@gmail.com>
2025-02-23 17:09:05 +0900

    DB構成変更

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,6 +17,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
+        'display_id',
         'name',
         'email',
         'password',
