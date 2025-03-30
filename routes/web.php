053ddf5a24e3d47e174d43c9def0c124077b00c8
ihsyamdev <ihsotas.yamasaki.dev@gmail.com>
2025-03-30 18:19:12 +0900

    CSRF除外のパスを修正

--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,18 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-// ユーザー関連エンドポイント
-Route::get('/users/{display_id}', [UserController::class, 'get']);
-Route::get('/users', [UserController::class, 'getAll']);
+// API関連エンドポイント
+Route::prefix('api')->group(function () {
+    // ユーザー関連エンドポイント
+    Route::get('/users/{display_id}', [UserController::class, 'get']);
+    Route::get('/users', [UserController::class, 'getAll']);
+    // 認証関連エンドポイント
+    Route::post('/auth/signup', [AuthController::class, 'signUp']);
+    Route::post('/auth/signin', [AuthController::class, 'signIn']);
+    Route::post('/auth/signout', [AuthController::class, 'signOut']);
+});
 
-// 認証関連エンドポイント
-Route::post('/auth/signup', [AuthController::class, 'signUp']);
-Route::post('/auth/signin', [AuthController::class, 'signIn']);
-Route::post('/auth/signout', [AuthController::class, 'signOut']);
-
-Route::get('/', function () {
+// ページ関連エンドポイント。SPAのため、全てのリクエストをReactに渡す
+Route::get('/{any}', function () {
     return view('welcome');
 })->where('any', '.*');
