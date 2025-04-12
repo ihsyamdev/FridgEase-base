4019b73832a8f0ce67669204d6384f1b9879f05b
ihsyamdev <ihsotas.yamasaki.dev@gmail.com>
2025-04-12 20:45:33 +0900

    ユーザー情報確認ページの実装

--- resources/js/pages/users/MyProfile.tsx
+++ resources/js/pages/users/MyProfile.tsx
@@ -5,15 +5,32 @@ export const MyProfile: React.FC = () => {
   const authContext = useContext(AuthContext)!
 
   return (
-    <div>
-      <label>username</label>
-      <input
-        type='text'
-        id='name'
-        name='name'
-        value={authContext.user?.name + 'ゆーざー' || ''}
-        className='w-full border border-gray-300 rounded-lg p-2' />
-
+    <div className='w-2/4 mx-auto'>
+      <h1 className='text-center font-bold text-2xl my-5'>
+        登録情報
+      </h1>
+      <div className='mb-5'>
+        <label htmlFor='name'>ユーザー名</label>
+        <input
+          type='text'
+          id='name'
+          name='name'
+          value={authContext.user?.name}
+          disabled
+          className='w-full border border-gray-300 rounded-lg p-2'
+        />
+      </div>
+      <div className='mb-5'>
+        <label htmlFor='email'>メールアドレス</label>
+        <input
+          type='email'
+          id='email'
+          name='email'
+          value={authContext.user?.email}
+          disabled
+          className='w-full border border-gray-300 rounded-lg p-2'
+        />
+      </div>
     </div>
   )
 }
