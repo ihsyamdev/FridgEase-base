8a563e8a2f20a2bd7e13b6c8db8fb09f74a811ea
ihsyamdev <ihsotas.yamasaki.dev@gmail.com>
2025-04-14 22:36:46 +0900

    Ingredientsのマイグレーションと、マイグレーションファイルの順番修正

--- database/migrations/2025_04_13_084752_create_ingredient.php
+++ database/migrations/2025_04_13_084752_create_ingredient.php
@@ -11,8 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('ingredient', function (Blueprint $table) {
+        Schema::create('ingredients', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('name');
+            $table->float('quantity');
+            $table->foreignId('unit_id')->constrained('units');
+            $table->date('expires_at')->nullable();
             $table->timestamps();
         });
     }
@@ -22,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('ingredient');
+        Schema::dropIfExists('ingredients');
     }
 };
